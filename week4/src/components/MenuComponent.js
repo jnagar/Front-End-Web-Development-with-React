9eb999ee2d364854a748846f51a1c80c3b977165
Jitendra Nagar <jnagar.iitk@gmail.com>
2019-06-04 17:19:48 +0530

    Redux Thunk

--- week4/src/components/MenuComponent.js
+++ week4/src/components/MenuComponent.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Card, CardImg, CardImgOverlay, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { Loading } from './LoadingComponent';
 
 function RenderMenuItem ({dish, onClick}) {
     return (
@@ -24,31 +25,50 @@ class Menu extends Component {
     }
 
     render() {
-        const menu = this.props.dishes.map((dish) => {
+        const menu = this.props.dishes.dishes.map((dish) => {
             return (
                 <div  className="col-12 col-md-5 m-1" key={dish.id}>
                     <RenderMenuItem dish={dish} onClick={this.props.onClick}/>
                 </div>
             );
         });
-
-        return (
-            <div className="container">
-                <div className="row">
-                    <Breadcrumb>
-                        <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
-                        <BreadcrumbItem active>Menu</BreadcrumbItem>
-                    </Breadcrumb>
-                    <div className="col-12">
-                        <h3>Menu</h3>
-                        <hr />
+        if (this.props.dishes.isLoading) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <Loading />
                     </div>
                 </div>
-                <div className="row">
-                    {menu}
+            );
+        }else if (this.props.dishes.errMess) {
+            return(
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <h4>{this.props.dishes.errMess}</h4>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        );
+            );
+        }else {
+            return (
+                <div className="container">
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to="/home">Home</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>Menu</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>Menu</h3>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                        {menu}
+                    </div>
+                </div>
+            );
+        }
     }
 }
 
