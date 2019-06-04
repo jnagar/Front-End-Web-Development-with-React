48e7c0f487dff9948079d0a629bfbba0a0f94f32
Jitendra Nagar <jnagar.iitk@gmail.com>
2019-06-04 18:22:19 +0530

    json-server setup

--- week4/src/components/ContactComponent.js
+++ week4/src/components/ContactComponent.js
@@ -123,7 +123,7 @@ class Contact extends Component{
                         <h3>Send us your Feedback</h3>
                     </div>
                     <div className="col-12 col-md-9">
-                        <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>
+                        <Form model="feedback" onSubmit={(values) => this.handleSubmit(values)}>cd
                             <Row className="form-group">
                                 <Label htmlFor="firstname" md={2}>First Name</Label>
                                 <Col md={10}>
